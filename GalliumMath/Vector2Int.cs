384fe9995313993c42bfaba1f0d85428bdbbfbcb
U-ACER-LAPTOP\stoik <stoiko.todorov@gmail.com>
2024-04-07 20:06:37 +0300

    Moving away from Unity math Reference Sources.

--- GalliumMath/Vector2Int.cs
+++ GalliumMath/Vector2Int.cs
@@ -5,260 +5,75 @@ using System.Runtime.InteropServices;
 
 namespace GalliumMath
 {
-    // Representation of 2D vectors and points.
-    [StructLayout(LayoutKind.Sequential)]
-    public struct Vector2Int : IEquatable<Vector2Int>, IFormattable
-    {
-        public int x
-        {
-            [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-            get { return m_X; }
+    
 
-            [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-            set { m_X = value; }
-        }
+public struct Vector2Int {
+    public int x, y;
 
+    public static readonly Vector2Int zero = new Vector2Int( 0, 0 );
+    public static readonly Vector2Int one = new Vector2Int( 1, 1 );
+    public static readonly Vector2Int up = new Vector2Int( 0, 1 );
+    public static readonly Vector2Int down = new Vector2Int( 0, -1 );
+    public static readonly Vector2Int left = new Vector2Int( -1, 0 );
+    public static readonly Vector2Int right = new Vector2Int( 1, 0 );
 
-        public int y
-        {
-            [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-            get { return m_Y; }
-
-            [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-            set { m_Y = value; } }
-
-        private int m_X;
-        private int m_Y;
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public Vector2Int(int x, int y)
-        {
-            m_X = x;
-            m_Y = y;
-        }
-
-        // Set x and y components of an existing Vector.
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public void Set(int x, int y)
-        {
-            m_X = x;
-            m_Y = y;
-        }
-
-        // Access the /x/ or /y/ component using [0] or [1] respectively.
-        public int this[int index]
-        {
-            [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-            get
-            {
-                switch (index)
-                {
-                    case 0: return x;
-                    case 1: return y;
-                    default:
-                        throw new IndexOutOfRangeException(String.Format("Invalid Vector2Int index addressed: {0}!", index));
-                }
-            }
-
-            [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-            set
-            {
-                switch (index)
-                {
-                    case 0: x = value; break;
-                    case 1: y = value; break;
-                    default:
-                        throw new IndexOutOfRangeException(String.Format("Invalid Vector2Int index addressed: {0}!", index));
-                }
-            }
-        }
-
-        // Returns the length of this vector (RO).
-        public float magnitude { [MethodImpl(MethodImplOptionsEx.AggressiveInlining)] get { return Mathf.Sqrt((float)(x * x + y * y)); } }
-
-        // Returns the squared length of this vector (RO).
-        public int sqrMagnitude { [MethodImpl(MethodImplOptionsEx.AggressiveInlining)] get { return x * x + y * y; } }
-
-        // Returns the distance between /a/ and /b/.
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static float Distance(Vector2Int a, Vector2Int b)
-        {
-            float diff_x = a.x - b.x;
-            float diff_y = a.y - b.y;
-
-            return (float)Math.Sqrt(diff_x * diff_x + diff_y * diff_y);
-        }
-
-        // Returns a vector that is made from the smallest components of two vectors.
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int Min(Vector2Int lhs, Vector2Int rhs) { return new Vector2Int(Mathf.Min(lhs.x, rhs.x), Mathf.Min(lhs.y, rhs.y)); }
-
-        // Returns a vector that is made from the largest components of two vectors.
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int Max(Vector2Int lhs, Vector2Int rhs) { return new Vector2Int(Mathf.Max(lhs.x, rhs.x), Mathf.Max(lhs.y, rhs.y)); }
-
-        // Multiplies two vectors component-wise.
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int Scale(Vector2Int a, Vector2Int b) { return new Vector2Int(a.x * b.x, a.y * b.y); }
-
-        // Multiplies every component of this vector by the same component of /scale/.
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public void Scale(Vector2Int scale) { x *= scale.x; y *= scale.y; }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public void Clamp(Vector2Int min, Vector2Int max)
-        {
-            x = Math.Max(min.x, x);
-            x = Math.Min(max.x, x);
-            y = Math.Max(min.y, y);
-            y = Math.Min(max.y, y);
-        }
-
-        // Converts a Vector2Int to a [[Vector2]].
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static implicit operator Vector2(Vector2Int v)
-        {
-            return new Vector2(v.x, v.y);
-        }
-
-        // Converts a Vector2Int to a [[Vector3Int]].
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static explicit operator Vector3Int(Vector2Int v)
-        {
-            return new Vector3Int(v.x, v.y, 0);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int FloorToInt(Vector2 v)
-        {
-            return new Vector2Int(
-                Mathf.FloorToInt(v.x),
-                Mathf.FloorToInt(v.y)
-            );
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int CeilToInt(Vector2 v)
-        {
-            return new Vector2Int(
-                Mathf.CeilToInt(v.x),
-                Mathf.CeilToInt(v.y)
-            );
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int RoundToInt(Vector2 v)
-        {
-            return new Vector2Int(
-                Mathf.RoundToInt(v.x),
-                Mathf.RoundToInt(v.y)
-            );
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int operator-(Vector2Int v)
-        {
-            return new Vector2Int(-v.x, -v.y);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int operator+(Vector2Int a, Vector2Int b)
-        {
-            return new Vector2Int(a.x + b.x, a.y + b.y);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int operator-(Vector2Int a, Vector2Int b)
-        {
-            return new Vector2Int(a.x - b.x, a.y - b.y);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int operator*(Vector2Int a, Vector2Int b)
-        {
-            return new Vector2Int(a.x * b.x, a.y * b.y);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int operator*(int a, Vector2Int b)
-        {
-            return new Vector2Int(a * b.x, a * b.y);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int operator*(Vector2Int a, int b)
-        {
-            return new Vector2Int(a.x * b, a.y * b);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static Vector2Int operator/(Vector2Int a, int b)
-        {
-            return new Vector2Int(a.x / b, a.y / b);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static bool operator==(Vector2Int lhs, Vector2Int rhs)
-        {
-            return lhs.x == rhs.x && lhs.y == rhs.y;
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static bool operator!=(Vector2Int lhs, Vector2Int rhs)
-        {
-            return !(lhs == rhs);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public override bool Equals(object other)
-        {
-            if (!(other is Vector2Int)) return false;
-
-            return Equals((Vector2Int)other);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public bool Equals(Vector2Int other)
-        {
-            return x == other.x && y == other.y;
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public override int GetHashCode()
-        {
-            return x.GetHashCode() ^ (y.GetHashCode() << 2);
-        }
-
-        /// *listonly*
-        public override string ToString()
-        {
-            return ToString(null, null);
-        }
-
-        public string ToString(string format)
-        {
-            return ToString(format, null);
-        }
-
-        public string ToString(string format, IFormatProvider formatProvider)
-        {
-            if (formatProvider == null)
-                formatProvider = CultureInfo.InvariantCulture.NumberFormat;
-            return string.Format("({0}, {1})", x.ToString(format, formatProvider), y.ToString(format, formatProvider));
-        }
-
-        public static Vector2Int zero { [MethodImpl(MethodImplOptionsEx.AggressiveInlining)] get { return s_Zero; } }
-        public static Vector2Int one { [MethodImpl(MethodImplOptionsEx.AggressiveInlining)] get { return s_One; } }
-        public static Vector2Int up { [MethodImpl(MethodImplOptionsEx.AggressiveInlining)] get { return s_Up; } }
-        public static Vector2Int down { [MethodImpl(MethodImplOptionsEx.AggressiveInlining)] get { return s_Down; } }
-        public static Vector2Int left { [MethodImpl(MethodImplOptionsEx.AggressiveInlining)] get { return s_Left; } }
-        public static Vector2Int right { [MethodImpl(MethodImplOptionsEx.AggressiveInlining)] get { return s_Right; } }
-
-        private static readonly Vector2Int s_Zero = new Vector2Int(0, 0);
-        private static readonly Vector2Int s_One = new Vector2Int(1, 1);
-        private static readonly Vector2Int s_Up = new Vector2Int(0, 1);
-        private static readonly Vector2Int s_Down = new Vector2Int(0, -1);
-        private static readonly Vector2Int s_Left = new Vector2Int(-1, 0);
-        private static readonly Vector2Int s_Right = new Vector2Int(1, 0);
+    public Vector2Int( int x, int y ) {
+        this.x = x;
+        this.y = y;
     }
+
+    public override bool Equals(object other) {
+        return other is Vector2Int ovi && Equals( ovi );
+    }
+
+    public bool Equals(Vector2Int other) {
+        return x == other.x && y == other.y;
+    }
+
+    public override int GetHashCode() {
+        return x.GetHashCode() ^ ( y.GetHashCode() << 2 );
+    }
+
+    public static implicit operator Vector2( Vector2Int v ) {
+        return new Vector2( v.x, v.y );
+    }
+
+    public static Vector2Int operator-( Vector2Int v ) {
+        return new Vector2Int( -v.x, -v.y );
+    }
+
+    public static Vector2Int operator+( Vector2Int a, Vector2Int b ) {
+        return new Vector2Int( a.x + b.x, a.y + b.y );
+    }
+
+    public static Vector2Int operator-( Vector2Int a, Vector2Int b ) {
+        return new Vector2Int( a.x - b.x, a.y - b.y );
+    }
+
+    public static Vector2Int operator*( Vector2Int a, Vector2Int b ) {
+        return new Vector2Int( a.x * b.x, a.y * b.y );
+    }
+
+    public static Vector2Int operator*( int a, Vector2Int b ) {
+        return new Vector2Int( a * b.x, a * b.y );
+    }
+
+    public static Vector2Int operator*( Vector2Int a, int b ) {
+        return new Vector2Int( a.x * b, a.y * b );
+    }
+
+    public static Vector2Int operator/( Vector2Int a, int b ) {
+        return new Vector2Int( a.x / b, a.y / b );
+    }
+
+    public static bool operator==( Vector2Int lhs, Vector2Int rhs ) {
+        return lhs.x == rhs.x && lhs.y == rhs.y;
+    }
+
+    public static bool operator!=( Vector2Int lhs, Vector2Int rhs ) {
+        return ! ( lhs == rhs );
+    }
+}
+
+
 }
