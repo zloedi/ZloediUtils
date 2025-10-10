6cb7f00449d33d08c9be8ff5653f29af51df3bd4
zloedi <stoiko.todorov@gmail.com>
2025-10-10 13:32:34 +0300

    Exposed LevenshteinDistance.

--- Cellophane.cs
+++ Cellophane.cs
@@ -316,50 +316,6 @@ static string SubstrToFirstDiff( string a, string b ) {
     return result;
 }
 
-static int LevenshteinDistance( string s, string t ) {
-    int n = s.Length;
-    int m = t.Length;
-    int[,] d = new int[n + 1, m + 1];
-
-    // Step 1
-    if (n == 0)
-    {
-        return m;
-    }
-
-    if (m == 0)
-    {
-        return n;
-    }
-
-    // Step 2
-    for (int i = 0; i <= n; d[i, 0] = i++)
-    {
-    }
-
-    for (int j = 0; j <= m; d[0, j] = j++)
-    {
-    }
-
-    // Step 3
-    for (int i = 1; i <= n; i++)
-    {
-        //Step 4
-        for (int j = 1; j <= m; j++)
-        {
-            // Step 5
-            int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
-
-            // Step 6
-            d[i, j] = Math.Min(
-                    Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
-                    d[i - 1, j - 1] + cost);
-        }
-    }
-    // Step 7
-    return d[n, m];
-}
-
 static string GetSorted( int k ) {
     return _allNames[_orderedDist[k] & 0xffff];
 }
@@ -1062,6 +1018,50 @@ public static bool VarChanged( string name, Type type = null ) {
     }
 
     return result;
+}
+
+public static int LevenshteinDistance( string s, string t ) {
+    int n = s.Length;
+    int m = t.Length;
+    int[,] d = new int[n + 1, m + 1];
+
+    // Step 1
+    if (n == 0)
+    {
+        return m;
+    }
+
+    if (m == 0)
+    {
+        return n;
+    }
+
+    // Step 2
+    for (int i = 0; i <= n; d[i, 0] = i++)
+    {
+    }
+
+    for (int j = 0; j <= m; d[0, j] = j++)
+    {
+    }
+
+    // Step 3
+    for (int i = 1; i <= n; i++)
+    {
+        //Step 4
+        for (int j = 1; j <= m; j++)
+        {
+            // Step 5
+            int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+            // Step 6
+            d[i, j] = Math.Min(
+                    Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                    d[i - 1, j - 1] + cost);
+        }
+    }
+    // Step 7
+    return d[n, m];
 }
 
 // replace existing cmds and vars with the ones coming from this assembly
